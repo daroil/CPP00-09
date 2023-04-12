3f8e9f5da06ab0534fa7acb80b23460867a1c0b7
Daroil <gendel2403@gmail.com>
2023-04-12 23:06:57 +0200

    some beautifications

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -10,38 +10,38 @@ class MyClass {       // The class
 
 int main(int argc, char **argv) 
 {
-    MyClass myObj;  // Create an object of MyClass
-    char    ch;
-    int     i;
-    size_t     j;
-    // Access attributes and set values
-     myObj.myNum = 15; 
-    myObj.myString = "Some text";
+	MyClass myObj;  // Create an object of MyClass
+	char    ch;
+	int     i;
+	size_t     j;
+	// Access attributes and set values
+	myObj.myNum = 15; 
+	myObj.myString = "Some text";
 
-    i = 1;
-    // Print attribute values
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    if (argc > 1)
-    {
-        while (i < argc)
-        {
-            j = 0;
-            while (j < std::strlen(argv[i]))
-            {
-                // if (std::isalpha(argv[i][j]))
-                std::cout << (ch = toupper(argv[i][j]));
-                // else
-                //     std::cout << (argv[i][j]);
-                j++;
-            }
-            if (argv[i + 1])
-                std::cout << " ";
-            i++;
-        }
+	i = 1;
+	// Print attribute values
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (j < std::strlen(argv[i]))
+			{
+				// if (std::isalpha(argv[i][j]))
+				std::cout << (ch = toupper(argv[i][j]));
+				// else
+				//     std::cout << (argv[i][j]);
+				j++;
+			}
+			if (argv[i + 1])
+				std::cout << " ";
+			i++;
+		}
     }
-    std::cout << "\n";
-    // std::cout << myObj.myNum << "\n";
-    // std::cout << myObj.myString << "\n";
-    return 0;
+	std::cout << "\n";
+	// std::cout << myObj.myNum << "\n";
+	// std::cout << myObj.myString << "\n";
+	return 0;
 }
