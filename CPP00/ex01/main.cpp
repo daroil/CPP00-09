ace1387e7a7c880cb4f901e285b70fcbd217f47a
Danila <gendel2403@gmail.com>
2023-04-26 11:59:06 +0200

    initial splitting into 3 separate actions, + some initial parsing

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dhendzel <dhendzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 02:55:21 by dhendzel          #+#    #+#             */
-/*   Updated: 2023/04/26 03:11:30 by dhendzel         ###   ########.fr       */
+/*   Updated: 2023/04/26 11:58:05 by dhendzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,21 @@ int main(void)
 
 	while(1)
 	{
+		//wait for prompt
+		//if prompt == exit - break
 		std::getline(std::cin,input);
 		if (input == "EXIT")
 		{
-			std::cout << "case 2" << std::endl;
+			std::cout << "case 1" << std::endl;
 			return(0);
-			
 		}
-		//wait for prompt
-		//if prompt == exit - break
-		//else if prompt == add exec add
-		//else if prompt == search exec search
-		//otherwise restart the loop
+		//else if prompt == add - exec add
+		else if (input == "ADD")
+			std::cout << "case 2" << std::endl;
+		//else if prompt == search - exec search
+		else if (input == "SEARCH")
+			std::cout << "case 3" << std::endl;
+		//otherwise continue the loop
 	}
 	return (0);
 }
