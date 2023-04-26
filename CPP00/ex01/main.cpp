f9041ef1807becbe728aeed0f45de30644f12b13
Danila <gendel2403@gmail.com>
2023-04-26 03:22:50 +0200

    started work with phonebook

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -1,8 +1,35 @@
-#include <iostream>
-#include <cstring>
-#include <cctype>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhendzel <dhendzel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 02:55:21 by dhendzel          #+#    #+#             */
+/*   Updated: 2023/04/26 03:11:30 by dhendzel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int argc, char **argv) 
+#include "phonebook.hpp"
+
+int main(void) 
 {
+	std::string	input;
+
+	while(1)
+	{
+		std::getline(std::cin,input);
+		if (input == "EXIT")
+		{
+			std::cout << "case 2" << std::endl;
+			return(0);
+			
+		}
+		//wait for prompt
+		//if prompt == exit - break
+		//else if prompt == add exec add
+		//else if prompt == search exec search
+		//otherwise restart the loop
+	}
 	return (0);
 }
---
+++ CPP00/ex01/phonebook.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhendzel <dhendzel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 02:54:07 by dhendzel          #+#    #+#             */
+/*   Updated: 2023/04/26 03:02:07 by dhendzel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+
+
+#endif
