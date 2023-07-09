3f7156c5df34a7efea366aee7666041f6f3538cb
Daroil <gendel2403@gmail.com>
2023-07-09 13:30:49 +0200

    copied work from ex00

---
+++ CPP05/ex01/main.cpp
@@ -0,0 +1,55 @@
+#include "Bureaucrat.hpp"
+#define GRADE	1
+
+int main() {
+    int ret;
+
+    ret = 0;
+    //TESTING CONSTRUCTION
+    std::cout << "\033[33m****** TESTING CONSTRUCTION *******\033[0m" << std::endl;
+    Bureaucrat *bureaucrat;
+
+    bureaucrat = NULL;
+    try {
+        bureaucrat = new Bureaucrat(GRADE, "LOL");
+        std::cout << bureaucrat << std::endl;
+    }
+    catch (Bureaucrat::GradeTooHighException &exception) {
+        std::cerr << "Bureaucrat instantiation failed: init grade " << GRADE << exception.what() << std::endl;
+        return (1);
+    }
+    catch (Bureaucrat::GradeTooLowException &exception) {
+        std::cerr << "Bureaucrat instantiation failed: init grade " << GRADE << exception.what() << std::endl;
+        return (1);
+    }
+
+    //TESTING INCREMENTAL
+    std::cout << "\033[33m****** TESTING INCREMENTAL *******\033[0m" << std::endl;
+    try {
+        bureaucrat->incrementGrade();
+        std::cout << bureaucrat << std::endl;
+    }
+    catch (Bureaucrat::GradeTooHighException &exception) {
+        std::cerr << "Bureaucrat incremental failed: result grade " << bureaucrat->getGrade() - 1 << exception.what() << std::endl;
+        ret = 1;
+    }
+
+    //TESTING DECREMENTAL
+    std::cout << "\033[33m****** TESTING DECREMENTAL *******\033[0m" << std::endl;
+    try {
+        bureaucrat->decrementGrade();
+        std::cout << bureaucrat << std::endl;
+    }
+    catch (Bureaucrat::GradeTooLowException &exception) {
+        std::cerr << "Bureaucrat decremental failed: result grade " << bureaucrat->getGrade() + 1  << exception.what() << std::endl;
+        ret = 1;
+    }
+
+    //DESTRUCTION
+    if (bureaucrat != NULL) {
+        std::cout << "\033[33m****** DESTRUCTION *******\033[0m" << std::endl;
+        delete bureaucrat;
+    }
+
+    return (ret);
+}
