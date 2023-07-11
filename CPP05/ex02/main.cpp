bd91b7f2c5f1c2403a7065def0ebd2fb30e9e7e6
Danila <gendel2403@gmail.com>
2023-07-11 14:17:33 +0200

    shrubbery creation form

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -1,14 +1,27 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 #define GRADE	1
 
+void    checkleaks(void)
+{
+    system("leaks ex02");
+}
+
 int main() {
+    atexit(checkleaks);
     PresidentialPardonForm first;
+    RobotomyRequestForm first2;
+    ShrubberyCreationForm first3;
     PresidentialPardonForm *second = new PresidentialPardonForm();
     PresidentialPardonForm *third = new PresidentialPardonForm("strange AForm");
     PresidentialPardonForm & formRef = first;
+    RobotomyRequestForm & formRef2 = first2;
+    ShrubberyCreationForm & formRef3 = first3;
     Bureaucrat guy(1, "cringe");
+
 
     first = *second;
     *second = *third;
@@ -18,7 +31,11 @@ int main() {
     std::cout << guy << std::endl;
     guy.signForm(third);
     guy.signForm(&formRef);
+    guy.signForm(&formRef2);
+    guy.signForm(&formRef3);
     guy.executeForm(formRef);
+    guy.executeForm(formRef2);
+    guy.executeForm(formRef3);
     delete second;
     delete  third;
     return (0);
