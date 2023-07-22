decac6267b4a81f8d8d7d732e14fb744efd23447
Danila <gendel2403@gmail.com>
2023-07-22 13:59:32 +0200

    started work ex02

---
+++ CPP06/ex02/Identify.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip> // std::fixed, std::setprecision()
+#include <cstring>
+#include <cstdlib> // std::strtol std::strtod
+#include <cctype> // isprint()
+#include <limits> // std::numeric_limits
+#include <math.h> // isnan(), isinf()
+
+class Identify
+{
+private:
+
+    Identify();
+    ~Identify();
+    Identify(Identify const &Identify);
+    Identify &operator=( Identify const &copy);
+
+public:
+
+};
---
+++ CPP06/ex02/main.cpp
@@ -0,0 +1,7 @@
+#include "Identify.hpp"
+
+int main(void)
+{
+
+    return 0;
+}
