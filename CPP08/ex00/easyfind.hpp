ea187d18ed64d87dddec32fab012541affa9241a
Danila <gendel2403@gmail.com>
2023-07-24 20:00:15 +0200

    we have to use STL algorithms, now I do

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -22,26 +22,26 @@
 //        return b;
 //}
 
-template <typename T>
-int easyfind(T a, int b)
-{
-    for (unsigned long i = 0; i < a.size(); i++)
-    {
-        if (a[i] == b)
-            return i;
-    }
-    throw std::exception();
-}
-
 //template <typename T>
-//typename T::iterator easyfind(T& container, int value) {
-//    // Find the first occurrence of 'value' in the container
-//    typename T::iterator it = std::find(container.begin(), container.end(), value);
-//
-//    // Check if the element was found
-//    if (it != container.end()) {
-//        return it;
-//    } else {
-//        throw std::runtime_error("Value not found in container");
+//int easyfind(T a, int b)
+//{
+//    for (unsigned long i = 0; i < a.size(); i++)
+//    {
+//        if (a[i] == b)
+//            return i;
 //    }
+//    throw std::exception();
 //}
+
+template <typename T>
+typename T::iterator easyfind(T& container, int value) {
+    // Find the first occurrence of 'value' in the container
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+
+    // Check if the element was found
+    if (it != container.end()) {
+        return it;
+    } else {
+        throw std::runtime_error("Value not found in container");
+    }
+}
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -28,10 +28,12 @@ int main(void)
 //        std::cout << std::endl;
 //
         try{
-//            std::vector<int>::iterator result = easyfind(myVector, 20);
-//            std::cout << std::distance(myVector.begin(), result) << std::endl;
-            std::cout << easyfind(myVector, 30) << std::endl;
-            std::cout << easyfind(myVector, 40) << std::endl;
+            std::vector<int>::iterator result = easyfind(myVector, 20);
+            std::cout << std::distance(myVector.begin(), result) << std::endl;
+            result = easyfind(myVector, 40);
+            std::cout << std::distance(myVector.begin(), result) << std::endl;
+//            std::cout << easyfind(myVector, 30) << std::endl;
+//            std::cout << easyfind(myVector, 40) << std::endl;
         }
         catch (std::exception &e){
             std::cout << "error" << std::endl;
