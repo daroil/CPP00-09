01d205250b4ea9394af85101861e6946377b08f0
Danila <gendel2403@gmail.com>
2023-07-27 17:10:24 +0200

    seems to pass subject tests

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,12 +6,18 @@
 #include <map>
 #include <string>
 
-std::map<std::string, float>    fillMapFromInput(std::string inputFile, char del)
+//struct DateComparator {
+//    bool operator()(const std::string& date1, const std::string& date2) const {
+//        return date1 < date2;
+//    }
+//};
+
+std::map<std::string, double>    fillMapFromInput(std::string inputFile, char del)
 {
-    std::map<std::string, float> rates;
+    std::map<std::string, double> rates;
     std::ifstream	infile(inputFile);
     std::string line;
-//    int i = 0;
+    int i = 0;
     while(std::getline(infile, line))
     {
         std::istringstream iss(line);
@@ -21,7 +27,7 @@ std::map<std::string, float>    fillMapFromInput(std::string inputFile, char del
         if(std::getline(iss, name, del) && (iss >> rate))
         {
 //            std::cout << i << " name " << name << " rate " << rate << std::endl;
-//            i++;
+            i++;
             rates[name] = rate;
         }
 //        else
@@ -30,7 +36,8 @@ std::map<std::string, float>    fillMapFromInput(std::string inputFile, char del
     return rates;
 }
 
-void    checkMapAgainstInput(std::string inputFile, std::map<std::string, float> rates)
+
+void    checkMapAgainstInput(std::string inputFile, std::map<std::string, double> rates)
 {
     std::ifstream	infile(inputFile);
     std::string line;
@@ -42,17 +49,38 @@ void    checkMapAgainstInput(std::string inputFile, std::map<std::string, float>
     {
         std::istringstream iss(line);
         std::string name;
-        float rate;
+        double rate;
         std::cout << std::fixed << std::setprecision(2);
         if(std::getline(iss, name, '|') && iss >> rate)
         {
             name.erase(std::remove_if(name.begin(), name.end(), ::isspace), name.end());
             if (rate < 0)
                 std::cerr << "Error: not a positive number"<< std::endl;
-            else if(rate >= static_cast<float>(std::numeric_limits<int>::max()))
+            else if(rate >= static_cast<double>(std::numeric_limits<int>::max()))
                 std::cerr << "Error: too large a number"<< std::endl;
             else
-                std::cout << name << " => " << rate << " = " << rate * rates[name] << std::endl;
+            {
+                if (rates[name])
+                    std::cout << name << " => " << rate << " = " << rates[name] << std::endl;
+                else
+                {
+                // Find the closest date that is smaller than the inputDate using lower_bound()
+                    std::map<std::string, double>::iterator it = rates.lower_bound(name);
+
+                    // Check if the iterator is not at the beginning of the map
+                    if (it != rates.begin()) {
+                        // Decrement the iterator to get the lower bound (closest smaller date)
+                        --it;
+
+                        // Now, 'it' points to the closest date that is smaller than the inputDate
+    //                    std::cout << "Closest smaller date: " << it->first << ", Value: " << it->second << std::endl;
+                        std::cout << name << " => " << rate << " = " << it->second * rate << std::endl;
+                    } else {
+                        // If the iterator is at the beginning, there is no smaller date in the map
+                        std::cout << "No smaller date found in the map." << std::endl;
+                    }
+                }
+            }
         }
         else
             std::cerr << "Error: bad input => " << name << std::endl;
@@ -67,11 +95,11 @@ int main(int argc, char **argv)
         std::cerr << "Error, wrong input" << std::endl;
         return (1);
     }
-    std::map<std::string, float> rates = fillMapFromInput("data.csv", ',');
-//    for (std::map<std::string, float>::iterator it = rates.begin(); it != rates.end(); ++it) {
+    std::map<std::string, double> rates = fillMapFromInput("data.csv", ',');
+//    for (std::map<std::string, float`>::iterator it = rates.begin(); it != rates.end(); ++it) {
 //        std::cout << it->first << "," << it->second << std::endl;
-//    }
-
+//    }`
+//    (void)argv;
     checkMapAgainstInput(argv[1], rates);
 //    std::map<std::string, float> input = fillMapFromInput(argv[1], '|');
 //    for (std::map<std::string, float>::iterator it = input.begin(); it != input.end(); ++it) {
