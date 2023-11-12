d8255fcf70932b2737e61f56d608493bffdab2e5
Danila <gendel2403@gmail.com>
2023-11-12 15:18:34 +0100

    deleted helper header

--- CPP09/ex02/include/helper.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <vector>
-#include <deque>
-#include <chrono>
-void printSequenceD(std::deque<int>& sequence);
-void mergeInsertSortD(std::deque<int>& sequence, int left, int right) ;
-void mergeD(std::deque<int>& sequence, int left, int mid, int right) ;
